a86856b3b2eaecf7a50080a44da4a096f2f4c4e5
Esmodea Burk <esmodearburk@gmail.com>
2023-04-18 20:05:22 -0500

    created basic (unfinished) load function

---
+++ main.lua
@@ -0,0 +1,49 @@
+-- require 'src/Dependencies'
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    math.randomseed(os.time())
+
+    love.window.setTitle('My Breakout')
+
+    gFonts = {
+        ['small'] = love.graphics.newFont('fonts/font.ttf', 8),
+        ['medium'] = love.graphics.newFont('fonts/font.ttf', 16),
+        ['large'] = love.graphics.newFont('fonts/font.ttf', 32),
+    }
+    love.graphics.setFont(gFonts['small'])
+
+    gTextures = {
+        ['background'] = love.graphics.newImage('graphics/background.png'),
+        ['main'] = love.graphics.newImage('graphics/breakout.png'),
+        ['arrows'] = love.graphics.newImage('graphics/arrows.png'),
+        ['hearts'] = love.graphics.newImage('graphics/hearts.png'),
+        ['particle'] = love.graphics.newImage('graphics/particle.png'),
+    }
+
+    -- push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    --     vsync = true,
+    --     fullscreen = false,
+    --     resizable = true
+    -- })
+
+    gSounds = {
+        ['paddle-hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall-hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static'),
+        ['confirm'] = love.audio.newSource('sounds/confirm.wav', 'static'),
+        ['select'] = love.audio.newSource('sounds/select.wav', 'static'),
+        ['no-select'] = love.audio.newSource('sounds/no-select.wav', 'static'),
+        ['brick-hit-1'] = love.audio.newSource('sounds/brick-hit-1.wav', 'static'),
+        ['brick-hit-2'] = love.audio.newSource('sounds/brick-hit-2.wav', 'static'),
+        ['hurt'] = love.audio.newSource('sounds/hurt.wav', 'static'),
+        ['victory'] = love.audio.newSource('sounds/victory.wav', 'static'),
+        ['recover'] = love.audio.newSource('sounds/recover.wav', 'static'),
+        ['high-score'] = love.audio.newSource('sounds/high_score.wav', 'static'),
+        ['pause'] = love.audio.newSource('sounds/pause.wav', 'static'),
+        
+        ['music'] = love.audio.newSource('sounds/music.wav', 'static'),
+    }
+
+end
