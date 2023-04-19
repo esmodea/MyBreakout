e46a15e9797235f273a72215de506428868c32cf
Esmodea Burk <esmodearburk@gmail.com>
2023-04-18 20:16:46 -0500

    finished first iteration of main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
--- require 'src/Dependencies'
+require 'src/Dependencies'
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -22,11 +22,11 @@ function love.load()
         ['particle'] = love.graphics.newImage('graphics/particle.png'),
     }
 
-    -- push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
-    --     vsync = true,
-    --     fullscreen = false,
-    --     resizable = true
-    -- })
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
 
     gSounds = {
         ['paddle-hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
@@ -46,4 +46,53 @@ function love.load()
         ['music'] = love.audio.newSource('sounds/music.wav', 'static'),
     }
 
+    gStateMachine = StateMachine {
+        ['start'] = function() return StartState() end
+    }
+    gStateMachine:change('start')
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.update(dt)
+    gStateMachine:update(dt)
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
+end
+
+function love.draw()
+    push:apply('start')
+    
+    local backgroundWidth = gTextures['background']:getWidth()
+    local backgroundHeight = gTextures['background']:getHeight() 
+
+    love.graphics.draw(gTextures['background'], 0, 0, 0, VIRTUAL_WIDTH / (backgroundWidth - 1), VIRTUAL_HEIGHT / (backgroundHeight - 1))
+
+    gStateMachine:render()
+
+    displayFPS()
+
+    push:apply('end')
+end
+
+function displayFPS()
+    love.graphics.setFont(gFonts['small'])
+    love.graphics.setColor(0, 1, 0, 1)
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 5, 5)
 end
