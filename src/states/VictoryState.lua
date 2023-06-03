6ba01c2604877ea19bb6ef135bda5b7630e0df70
Esmodea Burk <esmodearburk@gmail.com>
2023-06-02 20:15:30 -0500

    Created VictoryState render function

--- src/states/VictoryState.lua
+++ src/states/VictoryState.lua
@@ -25,3 +25,16 @@ function VictoryState:update(dt)
     end
 end
 
+function VictoryState:render()
+    self.paddle:render()
+    self.ball:render()
+
+    renderHealth(self.health)
+    renderScore(self.score)
+
+    love.graphics.setFont(gFonts['large'])
+    love.graphics.printf("Level " .. tostring(self.level) .. " complete!", 0, VIRTUAL_HEIGHT / 4, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(gFonts['medium'])
+    love.graphics.printf('Press Enter to serve!', 0, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
+end
