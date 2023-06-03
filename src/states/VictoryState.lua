19e7e02458c25e4e331f9ea7a6326fe6f3a4c24f
Esmodea Burk <esmodearburk@gmail.com>
2023-06-02 20:12:12 -0500

    Created Victory State enter and update functions

---
+++ src/states/VictoryState.lua
@@ -0,0 +1,27 @@
+VictoryState = Class{__includes = BaseState}
+
+function VictoryState:enter(params)
+    self.level = params.level
+    self.score = params.score
+    self.paddle = params.paddle
+    self.health = params.health
+    self.ball = params.ball
+end
+
+function VictoryState:update(dt)
+    self.paddle:update(dt)
+
+    self.ball.x = self.paddle.x + (self.paddle.width / 2) - 4
+    self.ball.y = self.paddle.y - 8
+
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+        gStateMachine:change('serve', {
+            level = self.level + 1,
+            bricks = LevelMaker.createMap(level), --change to self.level + 1 if buggy
+            paddle = self.paddle,
+            health = self.health,
+            score = self.score
+        })
+    end
+end
+
