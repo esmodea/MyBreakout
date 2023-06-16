14eb9b6247c9bcc93d599802763fe913c38fc1ed
Esmodea Burk <esmodearburk@gmail.com>
2023-06-16 12:25:53 -0500

    Created EnterHighScoreState update function

--- src/states/EnterHighScoreState.lua
+++ src/states/EnterHighScoreState.lua
@@ -13,3 +13,52 @@ function EnterHighScoreState:enter(params)
     self.score = params.score
     self.scoreIndex = params.scoreIndex
 end
+
+function EnterHighScoreState:update(dt)
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then 
+        local name = string.char(chars[1]) .. string.char(chars[2]) .. string.char(chars[3])
+
+        for i = 10, self.scoreIndex, -1 do  
+            self.highScores[i + 1] = {
+                name = self.highScores[i].name,
+                score = self.highScores[i].score
+            }
+        end
+
+        self.highScores[self.scoreIndex].name = name
+        self.highScores[self.scoreIndex].score = self.score
+
+        local scoresStr = ''
+
+        for i = 1, 10 do
+            scoresStr = scoresStr .. self.highScores[i].name .. '\n'
+            scoresStr = scoresStr .. tostring(self.highScores[i].score) .. '\n'
+        end
+
+        love.filesystem.write('breakout.1st', scoresStr)
+
+        gStateMachine:change('high-scores', {
+            highScores = self.highScores
+        })
+
+        if love.keyboard.wasPressed('left') and highlightedChar > 1 then
+            highlightedChar = highlightedChar - 1
+            gSounds['select']:play()
+        elseif love.keyboard.wasPressed('right') and highlightedChar < 3 then
+            highlightedChar = highlightedChar + 1
+            gSounds['select']:play()
+        end
+
+        if love.keyboard.wasPressed('up') then
+            chars[highlightedChar] = chars[highlightedChar] + 1
+            if chars[highlightedChar] > 90 then
+                chars[highlightedChar] = 65
+            end
+        elseif love.keyboard.wasPressed('down') then
+            chars[highlightedChar] = chars[highlightedChar] - 1
+            if chars[highlightedChar] < 65 then
+                chars[highlightedChar] = 90
+            end
+        end
+    end
+end
