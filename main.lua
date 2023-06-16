dda723975614dab8d3f80b22b0612f78dc83b33e
Esmodea Burk <esmodearburk@gmail.com>
2023-06-16 12:14:24 -0500

    Created init for EnterHighScoreState and added to state machine

--- main.lua
+++ main.lua
@@ -59,7 +59,8 @@ function love.load()
         ['serve'] = function() return ServeState() end,
         ['game-over'] = function() return GameOverState() end,
         ['victory'] = function() return VictoryState() end,
-        ['high-score'] = function() return HighScoreState() end
+        ['high-score'] = function() return HighScoreState() end,
+        ['enter-high-score'] = function() return EnterHighScoreState() end
     }
 
     gStateMachine:change('start', {
---
+++ src/states/EnterHighScoreState.lua
@@ -0,0 +1,15 @@
+EnterHighScoreState = Class{__includes = BaseState}
+
+local chars = {
+    [1] = 65,
+    [2] = 65,
+    [3] = 65
+}
+
+local highlightedChar = 1
+
+function EnterHighScoreState:enter(params)
+    self.highScores = params.highScores
+    self.score = params.score
+    self.scoreIndex = params.scoreIndex
+end
